6512816d7abeaec38985c0e1ae4b943d70567d3c
Buvaneshwari <thirubuvana@gmail.com>
2023-02-16 00:12:38 +0530

    contact

--- Src/components/contact/Contact.js
+++ Src/components/contact/Contact.js
@@ -1,11 +1,35 @@
-const Contact = ()=>{
-    return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>Address</p>
-            <p>Ph:1008 111 132</p>
-        </div>
-    )
+import './contact.css'
+import React,{useRef} from 'react'
+import {AiOutlineMail} from 'react-icons/ai'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com'
+
+function Contacts() {
+  const form = useRef();
+  const sendEmail =(e)=>{
+    e.preventDefault();
+    emailjs.sendForm('service_1sfivlv','template_95120a9',form.current,'ZEAxYiw_dfrhqw321')
+    e.target.reset()
+    alert("Message sent Sucessfully!! Thank you ")
+  }
+
+
+  return (
+    <section id='contacts'>    
+        
+      <div className=' contact-container'> 
+      <h2 className='contact-us'>Contact Us</h2>          
+        <form ref={form} onSubmit ={sendEmail} className="form-container">
+          <input type='text' name='name' placeholder='Your Name' required/>
+          <input type= 'email' name='email' placeholder='Your email-Id' required/>
+          <textarea type='message' name='message' placeholder='Your Message' required></textarea>
+          <button type='submit' className='contact-button'>Send Message</button>
+        </form>
+      </div>
+
+    </section>
+  )
 }
 
-export default Contact
+export default Contacts
